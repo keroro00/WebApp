b6b19133107ec89a61a6e029053d2d61bb4ee5db
keroro00 <82613217+keroro00@users.noreply.github.com>
2022-05-07 19:08:31 +0800

    Add files via upload

--- Proj/target/classes/META-INF/createLecture.sql
+++ Proj/target/classes/META-INF/createLecture.sql
@@ -1,6 +1,18 @@
+CREATE TABLE lectures (
+    lecture_id INTEGER NOT NULL,
+    lecture_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (lecture_id)
+);
+
 CREATE TABLE materials (
     material_id INTEGER NOT NULL,
     lecture_id INTEGER NOT NULL,
-    lecturename VARCHAR(50) NOT NULL,
-    content BLOB
+    file_name VARCHAR(50),
+    content BLOB,   
+    MIME VARCHAR(20),
+    PRIMARY KEY (material_id),
+    FOREIGN KEY (lecture_id) REFERENCES lectures(lecture_id)
 );
+
+
+INSERT INTO lectures VALUES (1,'testing');
