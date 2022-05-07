137b6c012f6976b31962009b28ec47f8a59bc5b5
keroro00 <82613217+keroro00@users.noreply.github.com>
2022-05-07 16:27:00 +0800

    Update createLecture.sql

--- Proj/src/main/resources/META-INF/createLecture.sql
+++ Proj/src/main/resources/META-INF/createLecture.sql
@@ -1,6 +1,17 @@
-CREATE TABLE materials (
-    material_id INTEGER NOT NULL,
-    lecture_id INTEGER NOT NULL,
-    lecturename VARCHAR(50) NOT NULL,
-    content BLOB
+CREATE TABLE lectures (
+    lecture_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    lecture_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (lecture_id)
 );
+
+CREATE TABLE materials (
+    material_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    lecture_id INTEGER NOT NULL,
+    file_name VARCHAR(50),
+    content BLOB,
+    PRIMARY KEY (material_id),
+    FOREIGN KEY (lecture_id) REFERENCES lectures(lecture_id)
+);
+
+
+INSERT INTO lectures(lecture_name) VALUES ('testing');
